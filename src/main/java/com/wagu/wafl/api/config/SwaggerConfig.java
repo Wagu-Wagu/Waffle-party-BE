60773ddd18b071ceb3ef2c290e5c4f2b3186b2f0
gahee99 <u1879@sookmyung.ac.kr>
2024-05-19 18:56:54 +0900

    feat: swagger 적용

---
+++ src/main/java/com/wagu/wafl/api/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.wagu.wafl.api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "[Waffle Party] dev API 명세서",
+                description = "Dev api 명세서",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        // "/v1/**" 경로에 매칭되는 API를 그룹화하여 문서화한다.
+        String[] paths = {"/api/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("Waffle Party API v1")  // 그룹 이름을 설정한다.
+                .pathsToMatch(paths)     // 그룹에 속하는 경로 패턴을 지정한다.
+                .build();
+    }
+}
--- src/main/java/com/wagu/wafl/api/domain/health/controller/HealthCheckController.java
+++ src/main/java/com/wagu/wafl/api/domain/health/controller/HealthCheckController.java
@@ -1,16 +1,32 @@
 package com.wagu.wafl.api.domain.health.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Tag(name = "HealthCheck", description = "Health check 관련 API 입니다.")
 @RestController
 @RequiredArgsConstructor
 public class HealthCheckController {
     private final Environment env;
 
-    @GetMapping("/health")
+    @Operation(
+            summary = "HealthCheck 확인",
+            description = "dev 서버가 살아 있는지 확인합니다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "서버 응답 가능",
+                    content = {@Content(schema = @Schema(implementation = String.class))})
+    })
+    @GetMapping("api/v1/health")
     public String test() {
         return "hello waffle dev server";
     }
