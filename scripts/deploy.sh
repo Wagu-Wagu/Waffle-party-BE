460e41864703999df902ea0e7852bec76d912970
김주영 <77871898+kjy-asl@users.noreply.github.com>
2024-05-20 00:35:40 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,14 +5,12 @@ JAR_NAME=$(basename $BUILD_PATH)
 IDLE_APPLICATION_PATH=/home/ubuntu/app/${JAR_NAME}
 TARGET_PORT=8080
 
-nohup java -jar -Duser.timezone=Asia/Seoul -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=dev $IDLE_APPLICATION_PATH >> /home/ubuntu/app/nohup.out 2>&1 &
+if [ "$DEPLOYMENT_GROUP_NAME" == "waguwagu-prod-codedeploy-group" ]
+then
+  nohup java -jar -Duser.timezone=Asia/Seoul -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=prod $IDLE_APPLICATION_PATH >> /home/ubuntu/app/nohup.out 2>&1 &
+fi
 
-# if [ "$DEPLOYMENT_GROUP_NAME" == "prod" ]
-# then
-#   nohup java -jar -Duser.timezone=Asia/Seoul -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=prod $IDLE_APPLICATION_PATH >> /home/ubuntu/app/nohup.out 2>&1 &
-# fi
-
-# if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
-# then
-#    nohup java -jar -Duser.timezone=Asia/Seoul -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=dev $IDLE_APPLICATION_PATH >> /home/ubuntu/app/nohup.out 2>&1 &
-# fi
+if [ "$DEPLOYMENT_GROUP_NAME" == "waguwagu-dev-codedeploy-group" ]
+then
+   nohup java -jar -Duser.timezone=Asia/Seoul -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=dev $IDLE_APPLICATION_PATH >> /home/ubuntu/app/nohup.out 2>&1 &
+fi
