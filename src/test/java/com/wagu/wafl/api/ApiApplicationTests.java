9410342e2c28f05ca60c1fc71aea537d91abebf3
gahee99 <u1879@sookmyung.ac.kr>
2024-05-19 18:32:01 +0900

    init: init project

---
+++ src/test/java/com/wagu/wafl/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagu.wafl.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
