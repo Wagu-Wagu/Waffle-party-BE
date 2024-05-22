90fd168caabcc5a9e55f53a6139cbabc2df705c9
kjy <gimjuyeong0620@gmail.com>
2024-05-22 10:46:37 +0900

    한글로 수정

--- src/main/java/com/wagu/wafl/api/domain/post/entity/OttTag.java
+++ src/main/java/com/wagu/wafl/api/domain/post/entity/OttTag.java
@@ -1,15 +1,15 @@
 package com.wagu.wafl.api.domain.post.entity;
 
 public enum OttTag {
-    NETFLIX("netflix"),
-    WATCHA("watcha"),
-    DISNEY("disney"),
-    WAVE("wave"),
-    TIVING("tiving"),
-    COUPANGPLAY("coupangplay"),
-    LAFTEL("laftel"),
-    NAVERSERIES("naverseries"),
-    ETC("etc");
+    NETFLIX("넷플릭스"),
+    WATCHA("왓챠"),
+    DISNEY("디즈니"),
+    WAVE("웨이브"),
+    TIVING("티빙"),
+    COUPANGPLAY("쿠팡플레이"),
+    LAFTEL("라프텔"),
+    NAVERSERIES("네이버시리즈"),
+    ETC("기타");
 
     private String ottTag;
 
