8527636c029c3a58ead73b43473b4092882abc8c
kjy <gimjuyeong0620@gmail.com>
2024-05-22 17:27:32 +0900

    이미지 요청은 제대로 됐지만 전달이 제대로 안 된 경우 예외처리

--- src/main/java/com/wagu/wafl/api/domain/s3/service/S3ServiceImpl.java
+++ src/main/java/com/wagu/wafl/api/domain/s3/service/S3ServiceImpl.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.wagu.wafl.api.common.exception.AwsException;
+import com.wagu.wafl.api.common.exception.PostException;
 import com.wagu.wafl.api.common.message.ExceptionMessage;
 import com.wagu.wafl.api.config.S3Config;
 import lombok.RequiredArgsConstructor;
@@ -29,21 +30,25 @@ public class S3ServiceImpl implements S3Service {
     @Override
     public String uploadImages(List<MultipartFile> multipartFiles, String folder) {
         List<String> s3Urls = new ArrayList<>();
-        multipartFiles.forEach(multipartFile -> {
-            String fileName = createFileName(multipartFile.getOriginalFilename());
-            ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentLength(multipartFile.getSize());
-            objectMetadata.setContentType(multipartFile.getContentType());
+        if (!multipartFiles.isEmpty()) {
+            multipartFiles.forEach(multipartFile -> {
+                String fileName = createFileName(multipartFile.getOriginalFilename());
+                ObjectMetadata objectMetadata = new ObjectMetadata();
+                objectMetadata.setContentLength(multipartFile.getSize());
+                objectMetadata.setContentType(multipartFile.getContentType());
 
-            String bucket = s3Config.getBucket();
-            try(InputStream inputStream = multipartFile.getInputStream()) {
-                amazonS3.putObject(new PutObjectRequest(bucket+"/"+folder+"/image", fileName, inputStream, objectMetadata)
-                        .withCannedAcl(CannedAccessControlList.PublicRead));
-                s3Urls.add(amazonS3.getUrl(bucket + "/" + folder + "/image", fileName).toString());
-            } catch (IOException e) {
-                throw new AwsException(ExceptionMessage.NOT_FOUND_IMAGE_TO_UPLOAD.getMessage(), HttpStatus.NOT_FOUND);
-            }
-        });
+                String bucket = s3Config.getBucket();
+                try (InputStream inputStream = multipartFile.getInputStream()) {
+                    amazonS3.putObject(new PutObjectRequest(bucket + "/" + folder + "/image", fileName, inputStream, objectMetadata)
+                            .withCannedAcl(CannedAccessControlList.PublicRead));
+                    s3Urls.add(amazonS3.getUrl(bucket + "/" + folder + "/image", fileName).toString());
+                } catch (IOException e) {
+                    throw new AwsException(ExceptionMessage.NOT_FOUND_IMAGE_TO_UPLOAD.getMessage(), HttpStatus.NOT_FOUND);
+                }
+            });
+        } else {
+            throw new PostException(ExceptionMessage.NOT_REQUESTED_FILE.getMessage(), HttpStatus.BAD_REQUEST);
+        }
         return s3Urls.stream()
                 .map(s3Url -> '"'+s3Url+'"')
                 .collect(Collectors.joining(",","[","]"));
