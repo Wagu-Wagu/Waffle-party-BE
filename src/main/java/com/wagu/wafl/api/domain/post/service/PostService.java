38acf76138c88b16c993c14c7d37b6d4d5554084
GaHee99 <u1879@sookmyung.ac.kr>
2024-05-24 16:27:32 +0900

    fix: 글 리스트 createdAt response수정

--- src/main/java/com/wagu/wafl/api/domain/post/service/PostService.java
+++ src/main/java/com/wagu/wafl/api/domain/post/service/PostService.java
@@ -3,7 +3,6 @@ package com.wagu.wafl.api.domain.post.service;
 import com.wagu.wafl.api.domain.post.dto.request.CreatePostRequestDTO;
 import com.wagu.wafl.api.domain.post.dto.response.OttPostsListResponseDTO;
 import com.wagu.wafl.api.domain.post.entity.OttTag;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
