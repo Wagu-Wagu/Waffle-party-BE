fc412e0ce0ab11e8079b90747e31fc2600482df6
kjy <gimjuyeong0620@gmail.com>
2024-05-24 16:20:10 +0900

    feat: 내가 작성한 글 목록 조회 api 개발

--- src/main/java/com/wagu/wafl/api/common/message/ResponseMessage.java
+++ src/main/java/com/wagu/wafl/api/common/message/ResponseMessage.java
@@ -14,7 +14,8 @@ public enum ResponseMessage {
     SUCCESS_EDIT_USER_NICKNAME("유저 닉네임 변경에 성공했습니다."),
     SUCCESS_EDIT_USER_PHOTO("유저 프로필 변경에 성공했습니다"),
     SUCCESS_GET_MY_INFO("유저 - 내정보 조회를 성공했습니다."),
-    SUCCESS_GET_MY_COMMENTS("유저 - 내가 작성한 댓글 정보 조회를 성공했습니다."),
+    SUCCESS_GET_MY_COMMENTS("유저 - 내가 작성한 댓글 리스트 정보 조회를 성공했습니다."),
+    SUCCESS_GET_MY_POSTS("유저 - 내가 작성한 글 리스트 정보 조회를 성공했습니다."),
     SUCCESS_USER_ONBOARD("유저 온보딩을 성공했습니다."),
     //post
     SUCCESS_CREATE_POST("게시글 생성을 성공했습니다."),
--- src/main/java/com/wagu/wafl/api/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/wagu/wafl/api/domain/user/service/UserServiceImpl.java
@@ -5,10 +5,12 @@ import com.wagu.wafl.api.common.message.ExceptionMessage;
 import com.wagu.wafl.api.config.S3Config;
 import com.wagu.wafl.api.config.WaguConfig;
 import com.wagu.wafl.api.domain.comment.entity.Comment;
+import com.wagu.wafl.api.domain.post.entity.Post;
 import com.wagu.wafl.api.domain.s3.service.S3ServiceImpl;
 import com.wagu.wafl.api.domain.user.dto.request.OnboardRequestDTO;
 import com.wagu.wafl.api.domain.user.dto.response.GetMyCommentResponseDTO;
 import com.wagu.wafl.api.domain.user.dto.response.GetMyInfoResponseDTO;
+import com.wagu.wafl.api.domain.user.dto.response.GetMyPostResponseDTO;
 import com.wagu.wafl.api.domain.user.entity.User;
 import com.wagu.wafl.api.domain.user.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -88,6 +90,20 @@ public class UserServiceImpl implements UserService{
         User user = findUser(userId);
 
         return GetMyInfoResponseDTO.of(user, waguConfig.getWaguVersion());
+    }
+
+    @Override
+    public List<GetMyPostResponseDTO> getMyPosts(Long userId) {
+        val user = userRepository.findUserAndPostOrderByCreatedAt(userId);
+
+        if(!user.isPresent()) {
+            return null;
+        }
+
+        List<Post> posts = user.get().getPosts();
+        return posts.stream().map(p -> {
+            return GetMyPostResponseDTO.of(p, user.get().getNickName());
+        }).collect(Collectors.toList());
     }
 
     @Override
