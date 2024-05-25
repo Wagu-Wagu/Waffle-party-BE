b82e986951933f3e38cac6eec0d7481e1254fb67
kjy <gimjuyeong0620@gmail.com>
2024-05-25 15:03:30 +0900

    fix : 유효한 토큰인지 예외 처리

--- src/main/java/com/wagu/wafl/api/config/resolver/UserIdResolver.java
+++ src/main/java/com/wagu/wafl/api/config/resolver/UserIdResolver.java
@@ -1,10 +1,13 @@
 package com.wagu.wafl.api.config.resolver;
 
+import com.wagu.wafl.api.common.exception.AuthException;
+import com.wagu.wafl.api.common.message.ExceptionMessage;
 import com.wagu.wafl.api.config.jwt.JwtTokenManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -27,15 +30,13 @@ public class UserIdResolver implements HandlerMethodArgumentResolver {
         final String token = request.getHeader("Authorization"); //이 부분도 위에 상수처리를 통해 매직넘버 지양하는게 맞는듯
                                                                         // 예시 ) private final AUTH_HEADER="Authoriziation"
 
-        if(!jwtTokenManager.verifyToken(token)) { //RunTimeException보다는, 커스텀 Exception이 나을 것 같음, 현재 RuntimeException ErrorHandler안잡혀있어서 500 에러남
-            throw new RuntimeException(String.format("USER_ID를 가져오지 못했습니다. (%s - %s", parameter.getClass(), parameter.getMethod()));
-        }
+        jwtTokenManager.verifyToken(token);
 
         final String tokenContents = jwtTokenManager.getJwtContents(token);
         try{
             return Long.parseLong(tokenContents);
         } catch (NumberFormatException e) { //에러 메시지 ExceptionMessage에 남기는게 나을 것 같음.
-            throw new RuntimeException(String.format("USER_ID를 가져오지 못했습니다. (%s - %s)", parameter.getClass(), parameter.getMethod()));
+            throw new AuthException(ExceptionMessage.NOT_PARSED_TO_ID.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 }
