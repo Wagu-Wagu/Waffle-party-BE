809768a3db7a4cd4245923ad0c5b85ad4cc67d0a
gahee99 <u1879@sookmyung.ac.kr>
2024-06-06 13:39:42 +0900

    fix: post detail 이미지 리스트 return 추가

--- src/main/java/com/wagu/wafl/api/domain/post/dto/response/PostDetailResponseDTO.java
+++ src/main/java/com/wagu/wafl/api/domain/post/dto/response/PostDetailResponseDTO.java
@@ -3,6 +3,9 @@ package com.wagu.wafl.api.domain.post.dto.response;
 import com.wagu.wafl.api.domain.comment.entity.Comment;
 import com.wagu.wafl.api.domain.post.entity.OttTag;
 import com.wagu.wafl.api.domain.post.entity.Post;
+import com.wagu.wafl.api.domain.post.util.PhotoesParsingToList;
+import java.util.ArrayList;
+import java.util.Objects;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -21,11 +24,19 @@ public record PostDetailResponseDTO(PostDetailVO postDetail, List<PostDetailComm
 
 @Builder
 record PostDetailVO(Long postId, String writerNickName, String userImage,
-                    OttTag ottTag, boolean isMyPost, LocalDateTime createdAt,
+                    OttTag ottTag, boolean isMyPost, List<String> photoes,
+                    LocalDateTime createdAt,
                     String title, String content, Long commentCount) {
+
     public static PostDetailVO of(Post post, boolean isMyPost) {
+        List<String> photoes = new ArrayList<>();
+
+        if(!Objects.isNull(post.getPhotoes()) &&!post.getPhotoes().isBlank()) {
+            photoes = PhotoesParsingToList.toList(post.getPhotoes());
+        }
         return PostDetailVO.builder()
                 .postId(post.getId())
+                .photoes(photoes)
                 .writerNickName(post.getUser().getNickName())
                 .userImage(post.getUser().getUserImage())
                 .ottTag(post.getOttTag())
