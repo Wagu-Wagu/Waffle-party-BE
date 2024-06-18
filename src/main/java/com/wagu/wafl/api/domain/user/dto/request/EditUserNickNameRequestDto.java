10c43e72b3c56c58d40d7837d7b0645e4a37b18c
gahee99 <u1879@sookmyung.ac.kr>
2024-06-18 09:47:51 +0900

    fix: 닉네임 자음, 모음도 가능하게 설정

--- src/main/java/com/wagu/wafl/api/domain/user/dto/request/EditUserNickNameRequestDto.java
+++ src/main/java/com/wagu/wafl/api/domain/user/dto/request/EditUserNickNameRequestDto.java
@@ -9,7 +9,7 @@ public record EditUserNickNameRequestDto (
 
         @NotNull(message = "유저 닉네임은 null일 수 없습니다.")
         @NotBlank(message = "유저 닉네임은 빈스트링 일 수 없습니다.")
-        @Pattern(regexp = "^[가-힣a-zA-Z0-9]*$", message = "사용자이름은 영어, 한글, 숫자만 가능합니다.")
+        @Pattern(regexp = "^[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9]*$", message = "사용자이름은 자음, 모음, 영어, 한글, 숫자만 가능합니다.")
         @Size(min = 1, max = 8)
        String nickName) {
 
