971b1201b664c2c9d635cbff9f7fc7a5ca191ea1
GaHee99 <u1879@sookmyung.ac.kr>
2024-06-26 17:30:44 +0900

    test: Service로직 testCode 작성가이드

--- src/test/java/com/wagu/wafl/api/domain/user/sevice/UserServiceImplTest.java
+++ src/test/java/com/wagu/wafl/api/domain/user/sevice/UserServiceImplTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,6 +33,7 @@ public class UserServiceImplTest {
     void 유저_생성_테스트() {
         // given
         User user = createUser();
+        // 스터빙한 메소드를 넣고, 어떤 동작을 넣을지 메소드 체이닝 형태로 작성
         when(userRepository.save(any(User.class))).thenReturn(user);
 
         // when
@@ -44,6 +44,15 @@ public class UserServiceImplTest {
                 () -> assertEquals(user.getEmail(), savedUser.getEmail(), "사용자 이메일이 일치해야 합니다.")
         );
     }
+    /*
+    참고 https://effortguy.tistory.com/143
+    thenReturn	스터빙한 메소드 호출 후 어떤 객체를 리턴할 건지 정의
+    thenThrow	스터빙한 메소드 호출 후 어떤 Exception을 Throw할 건지 정의
+    thenAnswer	스터빙한 메소드 호출 후 어떤 작업을 할지 custom하게 정의
+
+    mockito javadoc을 보면 이 메소드를 굳이 사용하지 말고 thenReturn, thenThrow 메소드 사용을 추천하고 있습니다.
+    thenCallRealMethod	실제 메소드 호출
+     */
 
 
     private User createUser() {
